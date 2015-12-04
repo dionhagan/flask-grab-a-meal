cf16716688ec2b5803c91c6133bad6c6584b249e
dionhagan <dionhagan@college.harvard.edu>
2015-12-04 03:50:38 -0500

    Added auth methods to User model for Flask-Login

--- app/models.py
+++ app/models.py
@@ -5,9 +5,28 @@ class User(db.Model):
     name = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     meals = db.relationship('Meal', backref='author', lazy='dynamic')
+    authenticated = db.Column(db.Boolean, default=False)
+
+    __tablename__ = 'user'
 
     def __repr__(self):
         return '<User %r>' % (self.name)
+
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.email
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
 
 class Meal(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
