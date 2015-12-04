af7d6f73269cc4688823ddf16a733d9682437c9a
Marina Adario <marinaadario@Marinas-MacBook-Pro.local>
2015-12-04 17:42:03 -0500

    added followers

--- app/models.py
+++ app/models.py
@@ -6,6 +6,12 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     meals = db.relationship('Meal', backref='author', lazy='dynamic')
     authenticated = db.Column(db.Boolean, default=False)
+    followed = db.relationship('User', 
+                               secondary=followers, 
+                               primaryjoin=(followers.c.follower_id == id), 
+                               secondaryjoin=(followers.c.followed_id == id), 
+                               backref=db.backref('followers', lazy='dynamic'), 
+                               lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % (self.name)
@@ -26,6 +32,19 @@ class User(db.Model):
         """False, as anonymous users aren't supported."""
         return False
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+            return self
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+            return self
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count()
+
 class Meal(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	house = db.Column(db.String(64))
@@ -35,3 +54,11 @@ class Meal(db.Model):
 
 	def __repr__(self):
 		return '<House: %r, Time: %r>' % (self.house, self.meal_time)
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+    )
+
+
+
