1c5c605cf01fc5c67b4c0db736e05fe7559fba6a
dionhagan <dionhagan@college.harvard.edu>
2015-12-05 16:57:37 -0500

    UI changes

--- app/views.py
+++ app/views.py
@@ -87,7 +87,7 @@ def plan():
                     'Hillel', 'Kirkland', 'Leverett', 'Lowell', 'Mather', 'Pforzheimer',
                     'Quincy', 'Winthrop']
         times = ['7:30 AM', '7:45 AM', '8:00 AM', '8:15 AM', '8:30 AM', '8:45 AM', '9:00 AM',
-                '9:15 AM', '9:30 AM', '9:45 AM', '10:00 AM', '11:30 AM', '11:45 AM', '12:00 PM', 
+                '9:15 AM', '9:30 AM', '9:45 AM', '10:00 AM', '11:30 AM', '11:45 AM', '12:00 PM',
                 '12:15 PM', '12:30 PM', '12:45 PM', '1:00 PM', '1:30 PM', '1:45 PM', '2:00 PM',
                 '2:15 PM', '2:30 PM', '5:00 PM', '5:15 PM', '5:30 PM', '5:45 PM', '6:00 PM',
                 '6:15 PM', '6:30 PM', '6:45 PM', '7:00 PM', '7:15 PM']
